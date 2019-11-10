3a515018c3efe858d48d78e9625c4336b1f578be
DinSchlimmsteAlptraum <flobo1991@gmail.com>
2019-11-10 16:29:39 +0100

    next step

---
+++ Calc/Calc/ICalculator.cs
@@ -0,0 +1,63 @@
+﻿namespace Calc
+{
+    public enum Operations
+    {
+        Addition = 1 ,
+        Subtraction = 2,
+        Multiplication = 3,
+        Division = 4,
+    }
+
+    public interface ICalculator
+    {
+        void AddOperand(int value);
+        void AddOperation(Operations operation);
+        void setTempVar1(int inTempvar1);
+    }
+
+    public class Calculator : ICalculator
+    {
+
+       private int TempVar1;
+       private int TempVar2;
+       private int sumary;
+
+        public void setTempVar1(int inTempVar1)
+        {   
+           
+        }
+        public void AddOperand(int value)
+        {
+          
+        }        
+
+        public void AddOperation(Operations operation)
+        {          
+
+        }
+
+        public string Calculate(Operations operations)
+        {
+            int i = (int)operations;
+            switch(i)
+            {
+                case 1:
+                    sumary = TempVar1 + TempVar2;
+                    return sumary.ToString();
+                case 2:
+                    sumary = TempVar1 - TempVar2;
+                    return sumary.ToString();
+                case 3:
+                    sumary = TempVar1 * TempVar2;
+                    return sumary.ToString();
+                case 4:
+                    sumary = TempVar1 / TempVar2;
+                    return sumary.ToString();
+                default:
+                    return null;
+            }
+
+        }
+
+    }
+}
--- Calc/Calc/MainPage.xaml.cs
+++ Calc/Calc/MainPage.xaml.cs
@@ -9,22 +9,122 @@ namespace Calc
 {
     public partial class MainPage : ContentPage
     {
+        private ICalculator calculator = new Calculator();
+
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+     
+
+        private void Button_ClickedC(object sender, EventArgs e)
         {
-          
+            
+            Result.Text = "";
+           
         }
 
-        private void Button_Clicked_1(object sender, EventArgs e)
+        private void Button_ClickedPlusMinus(object sender, EventArgs e)
         {
 
         }
 
-        private void Button_Clicked_2(object sender, EventArgs e)
+        private void Button_ClickedPercent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_ClickedDivide(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_ClickedSeven(object sender, EventArgs e)
+        {
+            calculator.AddOperand(7);
+            Result.Text += 7.ToString();
+        }
+
+        private void Button_ClickedEight(object sender, EventArgs e)
+        {
+            calculator.AddOperand(8);
+            Result.Text += 8.ToString();
+        }
+
+        private void Button_ClickedNine(object sender, EventArgs e)
+        {
+            calculator.AddOperand(9);
+            Result.Text += 9.ToString();
+        }
+
+        private void Button_ClickedMultiply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_ClickedFour(object sender, EventArgs e)
+        {
+            calculator.AddOperand(4);
+            Result.Text += 4.ToString();
+        }
+
+        private void Button_ClickedFive(object sender, EventArgs e)
+        {
+            calculator.AddOperand(5);
+            Result.Text += 5.ToString();
+        }
+
+        private void Button_ClickedSix(object sender, EventArgs e)
+        {
+            calculator.AddOperand(6);
+            Result.Text += 6.ToString();
+        }
+
+        private void Button_ClickedSubtract(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_ClickedOne(object sender, EventArgs e)
+        {
+            calculator.AddOperand(1);
+            Result.Text += 1.ToString();
+        }
+
+        private void Button_ClickedTwo(object sender, EventArgs e)
+        {
+            calculator.AddOperand(2);
+            Result.Text += 2.ToString();
+        }
+
+        private void Button_ClickedThree(object sender, EventArgs e)
+        {
+            calculator.AddOperand(3);
+            Result.Text += 3.ToString();
+        }
+
+        private void Button_ClickedAddition(object sender, EventArgs e)
+        {
+                    
+
+            calculator.AddOperation(Operations.Addition);
+            Result.Text += " + ".ToString();
+        }
+
+        private void Button_ClickedZero(object sender, EventArgs e)
+        {
+            calculator.AddOperand(0);
+            Result.Text += 0.ToString();
+        }
+
+
+        private void Button_ClickedResult(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button_ClickedPoint(object sender, EventArgs e)
         {
 
         }
